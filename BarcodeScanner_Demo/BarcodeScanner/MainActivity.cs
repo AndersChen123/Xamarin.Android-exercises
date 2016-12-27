c9eea7474bf986811cfa4dcaa96723de5f341de4
walt chen <hlcherb@hotmail.com>
2016-12-27 23:09:07 +0800

    Add BarcodeScanner demo and NumberNotify demo.

---
+++ BarcodeScanner_Demo/BarcodeScanner/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using ZXing;
+using ZXing.Mobile;
+
+namespace BarcodeScanner
+{
+    [Activity(Label = "BarcodeScanner", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private TextView _barcodeFormat, _barcodeData;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            MobileBarcodeScanner.Initialize(Application);
+
+            _barcodeFormat = FindViewById<TextView>(Resource.Id.barcode_format);
+            _barcodeData = FindViewById<TextView>(Resource.Id.barcode_data);
+
+            var button = FindViewById<Button>(Resource.Id.button_scan);
+            button.Click += async (sender, args) =>
+            {
+                var opts = new MobileBarcodeScanningOptions
+                {
+                    PossibleFormats = new List<BarcodeFormat>
+                    {
+                        BarcodeFormat.CODE_128,
+                        BarcodeFormat.EAN_13,
+                        BarcodeFormat.EAN_8,
+                        BarcodeFormat.QR_CODE
+                    }
+                };
+                var scanner = new MobileBarcodeScanner();
+                var result = await scanner.Scan(opts);
+
+                _barcodeFormat.Text = result?.BarcodeFormat.ToString() ?? string.Empty;
+                _barcodeData.Text = result?.Text ?? string.Empty;
+            };
+        }
+    }
+}
+
---
+++ NumberNotify_Demo/NumberNotify_Demo/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace NumberNotify_Demo
+{
+    [Activity(Label = "NumberNotify_Demo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        private uint _count = 0;
+        private TextView _numTextView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            
+            _numTextView = FindViewById<TextView>(Resource.Id.toolbar_cart_number);
+            if (_count == 0)
+            {
+                _numTextView.Visibility = ViewStates.Gone;
+            }
+
+            var cart = FindViewById<ImageView>(Resource.Id.toolbar_cart);
+            cart.Click += (sender, args) =>
+            {
+                _count += 1;
+                if (_numTextView.Visibility == ViewStates.Gone)
+                    _numTextView.Visibility = ViewStates.Visible;
+
+                if (_count < 10)
+                {
+                    _numTextView.Text = _count.ToString();
+                }
+                else
+                {
+                    _numTextView.Text = 9 + "+";
+                }
+            };
+        }
+    }
+}
+
